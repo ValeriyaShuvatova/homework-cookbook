618a6887f2558c9a96df0d59cbaa3037ff44895d
Valeriya Shuvatova <valeriyashuvatova.1983@gmail.com>
2024-11-18 16:59:22 +0300

    Add changes2

--- recipes.py
+++ recipes.py
@@ -1,80 +1,73 @@
-from importlib.metadata import files
-from operator import itemgetter
-from socket import socket
-
-cook_book = {}
-
-with open('recipes.txt', encoding='utf-8') as file:
-    last_key = ''
-    for line in file:
-        line = line.strip()
-        if line.isdigit():
-            continue
-        elif line and '|' not in line:
-            cook_book[line] = []
-            last_key = line
-        elif line and '|' in line:
-            name, qt, msure = line.split(" | ")
-            cook_book.get(last_key).append(dict(ingredient_name=name, quantity=int(qt), measure=msure))
-
-
-
-
-def get_shop_list_by_dishes(dishes, person_count):
-     ingr_list = {}
-     for line in dishes:
-         if line in cook_book:
-             for i in cook_book[line]:
-                meas_quan_list = {}
-                if i['ingredient_name'] not in ingr_list:
-                    meas_quan_list['measure'] = i['measure']
-                    meas_quan_list['quantity'] = i['quantity'] * person_count
-                    ingr_list[i['ingredient_name']] = meas_quan_list
-                else:
-                    ingr_list[i['ingredient_name']]['quantity'] = ingr_list[i['ingredient_name']]['quantity'] + \
-                                                                     i['quantity'] * person_count
-
-         else:
-            print(f'\n"Такого блюда нет в списке!"\n')
-     return ingr_list
-
-
-
+import os.path
 import os
 
-list = ("1.txt", "2.txt", "3.txt")
+#Задача №1
+with open("recipes.txt", encoding="utf-8") as file:
+    cook_book = {}
+    for i in file:
+        recepie_name = i.strip()
+        ingredients_count = file.readline()
+        ingredients =[]
+        for p in range(int(ingredients_count)):
+            recepie = file.readline().strip().split("|")
+            product, quantity, word = recepie
+            ingredients.append({"product": product, "quantity": quantity, "measure": word})
+        file.readline()
+        cook_book[recepie_name] = ingredients
+    print("cook_book =", *cook_book.items(), sep="\n")
+
+# Задача №2
+def get_shop_list_by_dishes(person_count: int, dishes: list):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for consist in cook_book[dish]:
+                if consist['product'] in result:
+                    result[consist["product"]]["quantity"] = consist["quantity"]*person_count
+                else:
+                    result[consist["product"]] = {"measure": consist["measure"], "quantity": (consist["quantity"]*person_count)}
+        else:
+            print("Такого блюда нет в книге")
+
+    print(result)
+get_shop_list_by_dishes(2, ["Запеченный картофель", "Омлет"])
 
 
+#Задача 3
+import os
 
-for item in list:
+
+all_files = os.listdir(r"C:\Users\lera\PycharmProjects\homework_cookbook\pythonProject2")
+txt_files = filter(lambda x: x[-4:] == ".txt", all_files)
+list = list(txt_files)[:3]
+
+for item in (list):
     list_final = []
-    with open("1.txt", 'r', encoding='utf-8') as f1:
-        file1 = f1.readlines()
-        list_general = ("1.txt", len(file1), file1)
-        list_final.append(list_general)
-    with open("2.txt", 'r', encoding='utf-8') as f2:
-        file2 = f2.readlines()
-        list_general = ("2.txt", len(file2), file2)
-        list_final.append(list_general)
-    with open("3.txt", 'r', encoding='utf-8') as f3:
-        file3 = f3.readlines()
-        list_general = ("3.txt", len(file3), file3)
+
+
+    with open(item, "r", encoding="utf-8") as file:
+
+        file = file.readlines()
+        list_general = item[0], len(file), file
         list_final.append(list_general)
 
-list_total = sorted(list_final, key=itemgetter(2), reverse=True)
-line = "\n".join(map(str, list_total))
+        list_total = sorted(list_final, key=itemgetter(2), reverse=True)
+
+        line = "".join(map(str, list_final))
+        print(line)
+
+
+    with open("rewrite_file.txt", 'a', encoding='utf-8') as f_total:
+        f_total.writelines(line)
 
 
 
-with open("rewrite_file.txt", 'w', encoding='utf-8') as f_total:
-    f_total.write(line)
 
 
-print("Задача №1")
-print("cook_book =", *cook_book.items(), sep="\n")
-print()
-print("Задача №2")
-print(get_shop_list_by_dishes(["Запеченный картофель", "Омлет"], 2), sep="\n")
-print()
-print("Задача №3")
+
+
+
+
+
+
 
