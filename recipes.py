a7e85e7293e2fa10993c36b7344471c45b4a89da
Valeriya Shuvatova <valeriyashuvatova.1983@gmail.com>
2024-11-25 13:24:43 +0300

    task1-2

--- recipes.py
+++ recipes.py
@@ -1,67 +1,48 @@
-import operator
-import os.path
-import os
+# Задание №1
 
-#Задача №1
-with open("recipes.txt", encoding="utf-8") as file:
+
+
+def create_cookbook():
     cook_book = {}
-    for i in file:
-        recepie_name = i.strip()
-        ingredients_count = file.readline()
-        ingredients =[]
-        for p in range(int(ingredients_count)):
-            recepie = file.readline().strip().split("|")
-            product, quantity, word = recepie
-            ingredients.append({"product": product, "quantity": int(quantity), "measure": word})
-        file.readline()
-        cook_book[recepie_name] = ingredients
-    print("cook_book =", *cook_book.items(), sep="\n")
+    with open("recipes.txt", encoding='utf-8') as fail:
 
-# Задача №2
-def get_shop_list_by_dishes(person_count: int, dishes: list):
-    result = {}
+        for i in fail:
+            recipe_name = i.strip()
+            ingredients_count = fail.readline()
+            list_ingredients = []
+            for j in range(int(ingredients_count)):
+                recipe = fail.readline().strip().split(' | ')
+                product, quantity, word = recipe
+                list_ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': word})
+            fail.readline()
+            cook_book[recipe_name] = list_ingredients
+        print("cook_book =", *cook_book.items(), sep="\n")
+        # print("cook_book =", create_cookbook())
+        return cook_book
+
+
+cook_book = create_cookbook()
+
+
+
+# Задание №2
+
+def get_shop_list_by_dishes(dishes: list, person_count: int):
+    shop_product = {}
     for dish in dishes:
-        if dish in cook_book:
-            for consist in cook_book[dish]:
-                if consist['product'] in result:
-                    result[consist["product"]]["quantity"] = consist["quantity"]*person_count
+        if dish in cook_book:  # Если ингридиент уже есть в словаре
+            for product in cook_book[dish]:  # {'ingredient_name': 'Картофель', 'quantity': '1', 'measure': 'кг'}
+                if product['ingredient_name'] in shop_product:
+                    shop_product[product['ingredient_name']]['quantity'] += int(product['quantity']) * person_count
                 else:
-                    result[consist["product"]] = {"measure": consist["measure"], "quantity": (consist["quantity"]*person_count)}
+                    shop_product[product['ingredient_name']] = {'measure': product['measure'],
+                                                                'quantity': int(product['quantity']) * person_count}
         else:
-            print("Такого блюда нет в книге")
-
-    print(result)
-get_shop_list_by_dishes(2, ["Запеченный картофель", "Омлет"])
+            print('Блюда нет в кулинарной книге')
+    print(shop_product)
 
 
-#Задача 3
-import os
-from operator import itemgetter
-
-
-all_files = [file for file in os.listdir() if file.endswith(".txt")]
-all_files = all_files[:3]
-list_final = []
-for item in all_files:
-
-    with open(item, "r", encoding="utf-8") as file:
-        read_file = file.readlines()
-        list_final.append([item, len(read_file), read_file])
-
-sorted_list_final = sorted(list_final, key=itemgetter(1))
-line = "\n".join(map(str, sorted_list_final))
-# print(line)
-with open("rewrite_file.txt", 'a', encoding='utf-8') as f_total:
-      f_total.writelines(line)
-
-
-
-
-
-
-
-
-
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
 
 
 
